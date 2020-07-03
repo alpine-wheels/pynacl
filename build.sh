cc9807d48c3e18231df0b809d6f2988bcfe9506d
William Jackson <william@subtlecoolness.com>
2020-07-03 11:02:54 -0500

    Simplify build script

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-rm -rf /workspace/pynacl
-/sbin/apk add --no-cache gcc git libffi-dev make musl-dev
-PYNACL_VERSION="$(/usr/bin/awk -F '==' '/pynacl/ { print $2 }' requirements.txt)"
-/usr/bin/git config --global advice.detachedHead false
-/usr/bin/git clone --branch "${PYNACL_VERSION}" --depth 1 https://github.com/pyca/pynacl.git
-cd pynacl || exit
-/usr/local/bin/python setup.py bdist_wheel
+#!/bin/sh
+
+set -e
+
+# install os dependencies
+apk add --no-cache gcc libffi-dev make musl-dev
+
+# build
+pip wheel --no-deps --requirement requirements.txt
